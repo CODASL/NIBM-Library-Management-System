54848994426306949ef48aa3ae1ef91d73f96daa
MukiBwoi <muksith20000309@gmail.com>
2022-04-16 21:07:03 +0530

    addded validations 3

--- Library Management System v1.1/View/Add RackNo.cs
+++ Library Management System v1.1/View/Add RackNo.cs
@@ -25,30 +25,39 @@ namespace Library_Management_System_v1._1.View
 
         private void btn_AddRackNo_Click(object sender, EventArgs e)
         {
-            Model.DatabaseService database = new Model.DatabaseService();
-            try
+            if (string.IsNullOrEmpty(txt_rackNo.Text))
             {
-                int row = database.insertData("INSERT INTO Rack VALUES('" + txt_rackId.Text + "','" + txt_rackNo.Text + "','" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "')");
-                if (row > 0)
+                Model.DatabaseService database = new Model.DatabaseService();
+                try
                 {
-                    this.Hide();
-                    Controller.CommonController.setActivity("Added " + txt_rackId.Text + " Rack No ");
-                    MessageBox.Show("Rack No Added Successfully");
-                    if (cmb_bookRacks != null)
+                    int row = database.insertData("INSERT INTO Rack VALUES('" + txt_rackId.Text + "','" + txt_rackNo.Text + "','" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "')");
+                    if (row > 0)
                     {
-                        Controller.BookController.loadComboBoxes(cmb_bookRacks, "Rack", "Rack_NO",AddBook.racks);
-                    }
-                    
-                }
-                else
-                {
-                    MessageBox.Show("Something Went Wrong");
-                }
+                        this.Hide();
+                        Controller.CommonController.setActivity("Added " + txt_rackId.Text + " Rack No ");
+                        MessageBox.Show("Rack No Added Successfully");
+                        if (cmb_bookRacks != null)
+                        {
+                            Controller.BookController.loadComboBoxes(cmb_bookRacks, "Rack", "Rack_NO", AddBook.racks);
+                        }
 
-            }catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Something Went Wrong");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
+            else
+            {
+                MessageBox.Show("Please enter rack number");
+            }
+            
         }
 
         private void btn_clearRackNo_Click(object sender, EventArgs e)
